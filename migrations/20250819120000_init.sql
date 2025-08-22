5c58766fe6e4836214be4709a87d534c68462294
Ori <git@ori.cat>
2025-08-23 01:57:44 +0200

    structure sql nicer

--- migrations/20250819120000_init.sql
+++ migrations/20250819120000_init.sql
@@ -18,5 +18,16 @@ CREATE TABLE IF NOT EXISTS levels (
     size INTEGER[],
     spawn INTEGER[],
     user_id UUID NOT NULL,
+		total_crystals INTEGER DEFAULT 0,
     FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+-- Create join table to track which users starred which levels
+CREATE TABLE IF NOT EXISTS level_stars (
+    user_id UUID NOT NULL,
+    level_id UUID NOT NULL,
+    starred_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (user_id, level_id),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (level_id) REFERENCES levels(id) ON DELETE CASCADE
 );
--- migrations/20250821170212_stars-and-crystals.sql
+++ migrations/20250821170212_stars-and-crystals.sql
@@ -1,15 +1,10 @@
-ALTER TABLE levels
-ADD COLUMN stars INTEGER DEFAULT 0;
+--this WILL cause inconsistencies, use Count instead
+-- https://www.w3schools.com/sql/sql_count.asp
+-- SELECT COUNT(*) FROM level_stars WHERE 
+--
+-- ALTER TABLE levels
+-- ADD COLUMN stars INTEGER DEFAULT 0;
 
 ALTER TABLE levels
 ADD COLUMN total_crystals INTEGER DEFAULT 0;
 
--- Create join table to track which users starred which levels
-CREATE TABLE IF NOT EXISTS level_stars (
-    user_id UUID NOT NULL,
-    level_id UUID NOT NULL,
-    starred_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    PRIMARY KEY (user_id, level_id),
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-    FOREIGN KEY (level_id) REFERENCES levels(id) ON DELETE CASCADE
-);
