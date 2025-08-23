29cbace10dd64b7f5f9a8dfd47425eb32f99cdff
Ori <git@ori.cat>
2025-08-23 02:06:10 +0200

    error catching

--- migrations/20250821170212_stars-and-crystals.sql
+++ migrations/20250821170212_stars-and-crystals.sql
@@ -5,6 +5,15 @@
 -- ALTER TABLE levels
 -- ADD COLUMN stars INTEGER DEFAULT 0;
 
-ALTER TABLE levels
-ADD COLUMN total_crystals INTEGER DEFAULT 0;
-
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1
+        FROM information_schema.columns
+        WHERE table_name='levels'
+          AND column_name='total_crystals'
+    ) THEN
+        ALTER TABLE levels ADD COLUMN total_crystals INTEGER DEFAULT 0;
+    END IF;
+END
+$$;
